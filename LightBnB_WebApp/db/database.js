19864a56b7e27d9450054c560226155b293235ed
semblant <robynhumphreys7_4@hotmail.com>
2024-12-22 21:54:27 -0700

    Refactor query

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -80,12 +80,10 @@ const getAllReservations = function (guest_id, limit = 10) {
         properties.*,
         reservations.guest_id
       FROM reservations
-      JOIN users ON users.id = reservations.guest_id
       JOIN properties ON properties.id = reservations.property_id
-      JOIN property_reviews ON reservations.id = property_reviews.reservation_id
       WHERE reservations.guest_id = $1
       AND reservations.end_date IS NOT NULL
-      GROUP BY properties.id, reservations.start_date, reservations.guest_id
+      GROUP BY properties.id, reservations.guest_id, reservations.start_date
       ORDER BY reservations.start_date
       LIMIT $2
       `, values
